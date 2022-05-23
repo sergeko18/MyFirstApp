693e8e20031f9aee7590691611ed4188e78e0346
Serge <zef1rqo@gmail.com>
2022-05-23 22:38:05 +0300

    add <Preloader />

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -3,12 +3,15 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
+
 
 let initialState = {
     users: [],
     pageSize: 5,
     totalUsersCount: 21,
     currentPage: 1,
+    isFetching: false
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -46,6 +49,9 @@ const usersReducer = (state = initialState, action) => {
         case SET_TOTAL_USERS_COUNT: {
             return {...state, totalUsersCount: action.count}
         }
+        case TOGGLE_IS_FETCHING: {
+            return {...state, isFetching: action.isFetching}
+        }
         default:
             return state;
     }
@@ -62,6 +68,7 @@ export const unfollowAC = (userId) => (
 export const setUsersAC = (users) => ({type: SET_USERS, users})
 export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
 export const setTotalUsersCountAC = (totalUserCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUserCount})
+export const toggleIsFetchingAC = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching})
 
 export default usersReducer;
 
---
+++ src/componets/common/Preloader/Preloader.jsx
@@ -0,0 +1,12 @@
+import preloader from "../../../assets/images/Iphone-spinner.gif";
+import React from "react";
+
+let Preloader = (props) => {
+    return (
+        <div>
+            <img src={preloader}/>
+        </div>
+    )
+}
+
+export default Preloader;
